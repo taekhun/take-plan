88afe858e09d2204be25f067d6b40d0deb1c0047
taekhun <nth9708@naver.com>
2020-12-23 18:52:16 +0900

    준비: connect heroku

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "src")));
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT);
